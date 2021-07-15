a8ef7ebd267983604157ab9f08a4160c60676828
Captain Jo <maticjosip2206@gmail.com>
2021-07-15 22:24:29 +0200

    update ui

--- js/app.js
+++ js/app.js
@@ -11,18 +11,32 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `linkFade 0.5s ease forwards ${
-          index / 7 + 0.8
-        }s`;
+        link.style.animation = `linkFade 0.5s ease forwards ${index / 7 + 0.8
+          }s`;
       }
     });
     //Burger animation
-    burger.classList.toggle('toggle')
+    burger.classList.toggle('toggle');
   });
 };
 
+const underlines = () => {
+  const homeButton = document.getElementById("home");
+  const projectsButton = document.getElementById("projects");
+  const galleryButton = document.getElementById("gallery");
+  const aboutButton = document.getElementById("about");
+  const line = document.getElementById("line")
+
+  homeButton.addEventListener("click", () => {
+    line.style.width = homeButton.style.minWidth
+    line.style.position = homeButton.style.position
+  });
+};
+
+
 const app = () => {
   navSlide();
+  underlines();
 };
 
 app();
