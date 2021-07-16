38c49a0ce4331b407e436c6212183396a3546d45
Captain Jo <maticjosip2206@gmail.com>
2021-07-16 11:36:11 +0200

    underline working

--- js/app.js
+++ js/app.js
@@ -25,14 +25,35 @@ const underlines = () => {
   const projectsButton = document.getElementById("projects");
   const galleryButton = document.getElementById("gallery");
   const aboutButton = document.getElementById("about");
-  const line = document.getElementById("line")
+  /* const offset = parseInt(window.getComputedStyle(links).marginLeft.replace("px", "")) - links.getBoundingClientRect().left; */
+  const offset = homeButton.getBoundingClientRect().left
+  var line = document.getElementById("line")
+
+  line.style.width = homeButton.clientWidth + 'px';
+  line.style.left = offset + 'px'
 
   homeButton.addEventListener("click", () => {
-    line.style.width = homeButton.style.minWidth
-    line.style.position = homeButton.style.position
+    moveLine(line, homeButton, offset)
+  });
+
+  projectsButton.addEventListener("click", () => {
+    moveLine(line, projectsButton, offset)
+  });
+
+  galleryButton.addEventListener("click", () => {
+    moveLine(line, galleryButton, offset)
+  });
+
+  aboutButton.addEventListener("click", () => {
+    moveLine(line, aboutButton, offset)
   });
 };
 
+function moveLine(lineElement, buttonElement, offset) {
+  lineElement.style.width = buttonElement.clientWidth + 'px';
+  lineElement.style.transform = 'translate(' + Math.ceil(buttonElement.getBoundingClientRect().left - offset) + 'px)';
+
+}
 
 const app = () => {
   navSlide();
