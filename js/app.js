a00b8e8970b8163fb0f3f58d04a7d6268d02acd1
Captain Jo <maticjosip2206@gmail.com>
2021-07-25 22:46:40 +0200

    gui update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+let counter = 0;
+var pages;
+var main;
+
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector("nav ul");
@@ -25,6 +29,7 @@ const underlines = () => {
   const projectsButton = document.getElementById("projects");
   const galleryButton = document.getElementById("gallery");
   const aboutButton = document.getElementById("about");
+  
   const offset = homeButton.getBoundingClientRect().left
   const lineWidth = homeButton.clientWidth
   var line = document.getElementById("line")
@@ -34,18 +39,22 @@ const underlines = () => {
 
   homeButton.addEventListener("click", () => {
     moveLine(line, homeButton, offset)
+    scrollH(0)
   });
 
   projectsButton.addEventListener("click", () => {
     moveLine(line, projectsButton, offset)
+    scrollH(1)
   });
 
   galleryButton.addEventListener("click", () => {
     moveLine(line, galleryButton, offset)
+    scrollH(2)
   });
 
   aboutButton.addEventListener("click", () => {
     moveLine(line, aboutButton, offset)
+    scrollH(3)
   });
 };
 
@@ -53,11 +62,30 @@ function moveLine(lineElement, buttonElement, offset) {
   lineElement.style.width = buttonElement.clientWidth + 'px';
   lineElement.style.transform = 'translate(' + Math.ceil(buttonElement.getBoundingClientRect().left - offset) + 'px)';
 
+};
+
+function scrollH(index) {
+  pages.style.transition = 'transform 0.5s ease-in-out';
+  pages.style.transform = 'translateX(' + (-100 * index) + 'vw)';
+};
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
 }
+
+const sectionParser = () => {
+  pages = document.querySelector('.content-container');
+  main = document.querySelector('main');
+};
 
 const app = () => {
   navSlide();
   underlines();
+  sectionParser();
 };
 
 app();
