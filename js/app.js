f3a3f38ee5b6d7cfc927aef1c21442747b08f2b9
Captain Jo <maticjosip2206@gmail.com>
2021-08-07 22:17:39 +0200

    major fixes, moved to jquery

--- js/app.js
+++ js/app.js
@@ -24,51 +24,6 @@ const navSlide = () => {
   });
 };
 
-const underlines = () => {
-  const homeButton = document.getElementById("home");
-  const projectsButton = document.getElementById("projects");
-  const galleryButton = document.getElementById("gallery");
-  const aboutButton = document.getElementById("about");
-  
-  const offset = homeButton.getBoundingClientRect().left
-  const lineWidth = homeButton.clientWidth
-  var line = document.getElementById("line")
-
-  line.style.width = lineWidth + 'px';
-  line.style.left = offset + 'px'
-
-  homeButton.addEventListener("click", () => {
-    moveLine(line, homeButton, offset)
-    scrollH(0)
-  });
-
-  projectsButton.addEventListener("click", () => {
-    moveLine(line, projectsButton, offset)
-    scrollH(1)
-  });
-
-  galleryButton.addEventListener("click", () => {
-    moveLine(line, galleryButton, offset)
-    scrollH(2)
-  });
-
-  aboutButton.addEventListener("click", () => {
-    moveLine(line, aboutButton, offset)
-    scrollH(3)
-  });
-};
-
-function moveLine(lineElement, buttonElement, offset) {
-  lineElement.style.width = buttonElement.clientWidth + 'px';
-  lineElement.style.transform = 'translate(' + Math.ceil(buttonElement.getBoundingClientRect().left - offset) + 'px)';
-
-};
-
-function scrollH(index) {
-  pages.style.transition = 'transform 0.5s ease-in-out';
-  pages.style.transform = 'translateX(' + (-100 * index) + 'vw)';
-};
-
 function sleep(milliseconds) {
   const date = Date.now();
   let currentDate = null;
@@ -77,15 +32,8 @@ function sleep(milliseconds) {
   } while (currentDate - date < milliseconds);
 }
 
-const sectionParser = () => {
-  pages = document.querySelector('.content-container');
-  main = document.querySelector('main');
-};
-
 const app = () => {
   navSlide();
-  underlines();
-  sectionParser();
 };
 
 app();
